6577969fab537a5fa7dc9b35cf49eabb94af5fa1
Daniele Mazzamuto <mazza.92@hotmail.it>
2023-10-03 11:40:52 +0200

    .env to hide api key (runtime config) implemented

--- server/api/basic.js
+++ server/api/basic.js
@@ -7,7 +7,8 @@ export default defineEventHandler(async (event) => {
   //const { age } = await readBody(event)
 
   // api call with private key
-  const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=cur_live_v9U84NVV5dWWOOfKO2DQrWyCGRhwkAE1utkZYri7')
+  const { currencyKey } = useRuntimeConfig();
+  const { data } = await $fetch(`https://api.currencyapi.com/v3/latest?apikey=${currencyKey}`)
   
   return {
     //message: `Hello ${name}! You are ${age} years old.`,
